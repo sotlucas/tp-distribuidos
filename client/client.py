602e529a7a46897bb4cae67929fecb959f9f39b3
Joaquin Prada <36085103+PradaJoaquin@users.noreply.github.com>
2023-09-29 23:36:57 -0300

    Implemented client to send csv file (#17)

---
+++ client/client.py
@@ -0,0 +1,55 @@
+import signal
+import socket
+import logging
+
+
+class Client:
+    def __init__(self, server_ip, server_port, file_path):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.file_path = file_path
+        # Register signal handler for SIGTERM
+        signal.signal(signal.SIGTERM, self.shutdown)
+
+    def run(self):
+        # Create a socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to the server
+        self.sock.connect((self.server_ip, self.server_port))
+        logging.info("Connected to server")
+
+        self.send_file()
+
+    def send_file(self):
+        """
+        Send the csv file line by line to the server.
+
+        Each line represents a flight with all the columns separated by commas.
+        """
+
+        logging.info("Sending file")
+        with open(self.file_path, "r") as f:
+            for line in f:
+                try:
+                    bytes = line.encode()
+                    # Add the \r\n\r\n sequence to mark the end of the message
+                    bytes += b"\r\n\r\n"
+                    bytes_to_send = len(bytes)
+
+                    while bytes_to_send > 0:
+                        # Send the data
+                        sent = self.sock.send(bytes)
+                        bytes_to_send -= sent
+                        bytes = bytes[sent:]
+
+                except OSError:
+                    # When receiving SIGTERM, the socket is closed and a OSError is raised.
+                    return
+        logging.info("File sent")
+        self.shutdown()
+
+    def shutdown(self):
+        logging.info("Shutting down")
+        self.sock.shutdown(socket.SHUT_WR)
+        self.sock.close()
+        logging.info("Shut down completed")
