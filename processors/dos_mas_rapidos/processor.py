cc0954e7ec0044b4a392da5be1130fcc91aab4dd
Lucas Sotelo <lucasnsotelog@gmail.com>
2023-10-05 00:40:57 -0300

    feat: processor dos mas rapidos

---
+++ processors/dos_mas_rapidos/processor.py
@@ -0,0 +1,40 @@
+TRAVEL_DURATION_INDEX = 3
+
+
+class Processor:
+    def __init__(self):
+        self.fastest = []
+
+    def proccess(self, message):
+        """
+        Checks if the travel duration is one of the two fastest and if it is,
+        it adds the message to the fastest list
+        """
+        travel_duration = self.convert_message_to_travel_duration(message)
+        if len(self.fastest) < 2:
+            self.fastest.append(message)
+        else:
+            second_fastest = self.fastest[1].split(",")[TRAVEL_DURATION_INDEX]
+            if travel_duration < self.convert_travel_duration(second_fastest):
+                self.fastest[1] = message
+        self.fastest.sort(key=self.convert_message_to_travel_duration)
+
+    def convert_message_to_travel_duration(self, message):
+        """
+        Converts a message to a travel duration in minutes
+        """
+        params = message.split(",")
+        return self.convert_travel_duration(params[TRAVEL_DURATION_INDEX])
+
+    def convert_travel_duration(self, travel_duration):
+        """
+        Converts the travel duration to minutes.
+        The input format is PT1H30M and the output format is 90
+        """
+        hours = 0
+        minutes = 0
+        if "H" in travel_duration:
+            hours = int(travel_duration.split("H")[0].split("T")[1])
+        if "M" in travel_duration:
+            minutes = int(travel_duration.split("M")[0].split("H")[1])
+        return hours * 60 + minutes
