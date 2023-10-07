ae99d2bcb0a8d2463f3636310f6ca7bc85dd0409
Lucas Sotelo <lucasnsotelog@gmail.com>
2023-10-07 16:30:51 -0300

    test: setup tests

--- processors/dos_mas_rapidos/processor_test.py
+++ processors/dos_mas_rapidos/processor_test.py
@@ -1,7 +1,7 @@
 from processor import Processor
 
 
-def processor_dos_mas_rapidos():
+def test_processor_dos_mas_rapidos():
     vuelos = [
         "9ca,ATL,BOS,PT1H20M,BOS",
         "0e8,PHL,DFW,PT2H20M,DFW",
@@ -15,7 +15,3 @@ def processor_dos_mas_rapidos():
     assert 2 == len(processor.fastest)
     assert "gf4,DFW,ATL,PT0H30M,ATL" == processor.fastest[0]
     assert "9ca,ATL,BOS,PT1H20M,BOS" == processor.fastest[1]
-
-
-if __name__ == '__main__':
-    processor_dos_mas_rapidos()
