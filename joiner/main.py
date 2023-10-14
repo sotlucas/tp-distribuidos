9145445ac6d0cc69f54853a86b4bece3c97035c9
Joaquin Prada <joaquiprada@gmail.com>
2023-10-14 19:37:51 -0300

    Joiner now can replicate & scaled other components
    Co-authored-by: Lucas Sotelo <lsotelo@fi.uba.ar>

--- joiner/main.py
+++ joiner/main.py
@@ -3,16 +3,22 @@ from commons.log_initializer import initialize_log
 from commons.config_initializer import initialize_config
 from commons.communication_initializer import CommunicationInitializer
 
+# Because they are working as an exchange, they do not share the same as the other groupers
+# TODO: See if this can be changed
+JOINER_REPLICA_COUNT = 1
+
 
 def main():
     config_inputs = {
+        "replica_id": int,
         "lat_long_input": str,
         "vuelos_input": str,
         "output": str,
         "logging_level": str,
         "rabbit_host": str,
         "output_type": str,
-        "input_type": str,
+        "input_type_lat_long": str,
+        "input_type_vuelos": str,
         "replicas_count": int,
     }
     config_params = initialize_config(config_inputs)
@@ -25,8 +31,9 @@ def main():
     )
     lat_long_receiver = lat_long_communication_initializer.initialize_receiver(
         config_params["lat_long_input"],
-        config_params["input_type"],
-        config_params["replicas_count"],
+        config_params["input_type_lat_long"],
+        JOINER_REPLICA_COUNT,
+        input_diff_name=str(config_params["replica_id"]),
     )
 
     vuelos_communication_initializer = CommunicationInitializer(
@@ -34,7 +41,7 @@ def main():
     )
     vuelos_receiver = vuelos_communication_initializer.initialize_receiver(
         config_params["vuelos_input"],
-        config_params["input_type"],
+        config_params["input_type_vuelos"],
         config_params["replicas_count"],
     )
     vuelos_sender = vuelos_communication_initializer.initialize_sender(
