f9dddd5b8083f5ed8f875ed399c90e5d8c608395
Joaquin Prada <joaquiprada@gmail.com>
2023-11-08 01:07:36 -0300

    Refactor working for max_avg

---
+++ processors/max_avg/max_avg.py
@@ -0,0 +1,25 @@
+class MaxAvg:
+    def process(self, message):
+        # input message: route;prices
+        # output message: route,avg,max_price
+
+        route = message["route"]
+        prices = message["prices"]
+        prices = [float(price) for price in prices.split(";")]
+
+        # 1. Calcula el avg y max de los precios.
+        avg = self.get_avg(prices)
+        max_price = self.get_max(prices)
+
+        # 2. Formateo el resultado de salida.
+        message = {"route": route, "avg": avg, "max_price": max_price}
+        return message
+
+    def get_avg(self, prices):
+        return sum(prices) / len(prices)
+
+    def get_max(self, prices):
+        return max(prices)
+
+    def finish_processing(self):
+        pass
