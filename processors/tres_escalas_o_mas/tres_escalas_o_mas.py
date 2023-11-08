a67b95d98d841805b8e224e7fd690b76a8697b8c
Joaquin Prada <joaquiprada@gmail.com>
2023-11-08 01:36:29 -0300

    Refactor working for tres escalas o mas

---
+++ processors/tres_escalas_o_mas/tres_escalas_o_mas.py
@@ -0,0 +1,15 @@
+SEGMENTS_ARRIVAL_AIRPORT = "segmentsArrivalAirportCode"
+
+
+class TresEscalasOMas:
+    def process(self, message):
+        segmentsArrivalAirportCode = message[SEGMENTS_ARRIVAL_AIRPORT]
+        arrivals = segmentsArrivalAirportCode.split("||")
+        stopover = len(arrivals) - 1  # -1 because the last arrival is the destination
+        if stopover >= 3:
+            return message
+        else:
+            return None
+
+    def finish_processing(self):
+        pass
