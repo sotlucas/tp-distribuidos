7dbc59e3b216838f86fbce8461eabeb891ba0c06
Lucas Sotelo <lucasnsotelog@gmail.com>
2023-11-25 13:41:57 -0300

    feat: filter general replicas count

--- health_checker/health_checker.py
+++ health_checker/health_checker.py
@@ -2,42 +2,65 @@ import signal
 import socket
 import logging
 import time
+from multiprocessing import Process
+
+HEALTH_CHECKER_PORT = 5000
+
+
+class HealthCheckerConfig:
+    def __init__(self, filter_general_replicas):
+        self.filter_general_replicas = filter_general_replicas
 
 
 class HealthChecker:
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
         self.running = True
         # Register signal handler for SIGTERM
         signal.signal(signal.SIGTERM, self.__stop)
 
     def run(self):
         """
-        Runs the server and accepts new connections.
+        Starts the health checker.
         """
-        # Call all the processors to check if they are healthy
+        # Call all the processors to check if they are healthy. Each in a new process.
+        processor_checkers = []
+        for i in range(1, self.config.filter_general_replicas + 1):
+            processor_checker = Process(target=self.check_processor, args=(i,))
+            processor_checker.start()
+            processor_checkers.append(processor_checker)
 
+        # Wait for the processes to finish
+        for processor_checker in processor_checkers:
+            processor_checker.join()
+            logging.info("Processor checker finished")
+
+    def check_processor(self, processor_id):
+        """
+        Checks if a processor is healthy.
+        """
         while True:
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect(("tp1-filter_general-3", 5000))
+                sock.connect((f"tp1-filter_general-{processor_id}", HEALTH_CHECKER_PORT))
                 break
             except socket.error:
                 logging.info("Connection Failed, Retrying..")
-                time.sleep(1)
-        logging.info("Connected to processor")
+                time.sleep(2)
+        logging.info(f"Connected to processor {processor_id}")
         while self.running:
             try:
                 sock.sendall(b"CHECK\n")
-                logging.info("Sent check message")
+                logging.info(f"Sent check message to processor {processor_id}")
                 # While loop to read from socket to avoid short reads
                 data = b""
                 while b"\n" not in data:
                     data += sock.recv(1024)
-                logging.info("Received data")
+                logging.info(f"Received data from processor {processor_id}")
                 if data == b"OK\n":
-                    logging.info("Processor is healthy")
+                    logging.info(f"Processor {processor_id} is healthy")
                 else:
-                    logging.error("Processor is not healthy")
+                    logging.error(f"Processor {processor_id} is not healthy")
                 time.sleep(5)
             except OSError as e:
                 logging.exception(f"Error: {e}")
--- health_checker/main.py
+++ health_checker/main.py
@@ -1,4 +1,4 @@
-from health_checker import HealthChecker
+from health_checker import HealthChecker, HealthCheckerConfig
 from commons.log_initializer import initialize_log
 from commons.config_initializer import initialize_config
 
@@ -6,13 +6,18 @@ from commons.config_initializer import initialize_config
 def main():
     config_inputs = {
         "logging_level": str,
+        "filter_general_replicas": int,
     }
     config_params = initialize_config(config_inputs)
 
     logging_level = config_params["logging_level"]
     initialize_log(logging_level)
 
-    HealthChecker().run()
+    config = HealthCheckerConfig(
+        config_params["filter_general_replicas"],
+    )
+
+    HealthChecker(config).run()
 
 
 if __name__ == "__main__":
