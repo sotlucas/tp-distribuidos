90d7f7efea53a891f994071b04c6dee6db2de34d
Lucas Sotelo <lucasnsotelog@gmail.com>
2023-11-26 14:28:34 -0300

    feat: all processors in health checker

--- health_checker/health_checker.py
+++ health_checker/health_checker.py
@@ -8,14 +8,38 @@ import docker
 from commons.protocol import CommunicationBuffer, Message, PeerDisconnected
 
 HEALTH_CHECKER_PORT = 5000
-CONNECTION_RETRY_TIME = 2
-HEALTH_CHECK_INTERVAL = 5
+CONNECTION_RETRY_TIME = 5
+HEALTH_CHECK_INTERVAL = 20
 
 
 class HealthCheckerConfig:
-    def __init__(self, filter_general_replicas, filter_multiple_replicas):
+    def __init__(self, filter_general_replicas, filter_multiple_replicas,
+                 filter_avg_max_replicas, filter_distancia_replicas,
+                 filter_tres_escalas_o_mas_replicas, filter_dos_mas_rapidos_replicas,
+                 filter_lat_long_replicas, processor_tres_escalas_o_mas_replicas,
+                 processor_dos_mas_rapidos_replicas, processor_distancias_replicas,
+                 processor_max_avg_replicas, processor_media_general_replicas, tagger_dos_mas_rapidos_replicas,
+                 tagger_tres_escalas_o_mas_replicas, tagger_distancias_replicas,
+                 tagger_max_avg_replicas, load_balancer_replicas, grouper_replicas, joiner_replicas):
         self.filter_general_replicas = filter_general_replicas
         self.filter_multiple_replicas = filter_multiple_replicas
+        self.filter_avg_max_replicas = filter_avg_max_replicas
+        self.filter_distancia_replicas = filter_distancia_replicas
+        self.filter_tres_escalas_o_mas_replicas = filter_tres_escalas_o_mas_replicas
+        self.filter_dos_mas_rapidos_replicas = filter_dos_mas_rapidos_replicas
+        self.filter_lat_long_replicas = filter_lat_long_replicas
+        self.processor_tres_escalas_o_mas_replicas = processor_tres_escalas_o_mas_replicas
+        self.processor_dos_mas_rapidos_replicas = processor_dos_mas_rapidos_replicas
+        self.processor_distancias_replicas = processor_distancias_replicas
+        self.processor_max_avg_replicas = processor_max_avg_replicas
+        self.processor_media_general_replicas = processor_media_general_replicas
+        self.tagger_dos_mas_rapidos_replicas = tagger_dos_mas_rapidos_replicas
+        self.tagger_tres_escalas_o_mas_replicas = tagger_tres_escalas_o_mas_replicas
+        self.tagger_distancias_replicas = tagger_distancias_replicas
+        self.tagger_max_avg_replicas = tagger_max_avg_replicas
+        self.load_balancer_replicas = load_balancer_replicas
+        self.grouper_replicas = grouper_replicas
+        self.joiner_replicas = joiner_replicas
 
 
 class HealthChecker:
@@ -34,20 +58,44 @@ class HealthChecker:
         """
         # Call all the processors to check if they are healthy. Each in a new process.
         processor_checkers = []
-        for i in range(1, self.config.filter_general_replicas + 1):
-            processor_checker = Process(target=self.check_processor, args=(f"tp1-filter_general-{i}",))
-            processor_checker.start()
-            processor_checkers.append(processor_checker)
-
-        for i in range(1, self.config.filter_multiple_replicas + 1):
-            processor_checker = Process(target=self.check_processor, args=(f"tp1-filter_multiple-{i}",))
-            processor_checker.start()
-            processor_checkers.append(processor_checker)
+        self.init_checker("tp1-filter_general-", self.config.filter_general_replicas, processor_checkers)
+        self.init_checker("tp1-filter_multiple-", self.config.filter_multiple_replicas, processor_checkers)
+        self.init_checker("tp1-filter_avg_max-", self.config.filter_avg_max_replicas, processor_checkers)
+        self.init_checker("tp1-filter_distancia-", self.config.filter_distancia_replicas, processor_checkers)
+        self.init_checker("tp1-filter_tres_escalas_o_mas-", self.config.filter_tres_escalas_o_mas_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-filter_dos_mas_rapidos-", self.config.filter_dos_mas_rapidos_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-filter_lat_long-", self.config.filter_lat_long_replicas, processor_checkers)
+        self.init_checker("tp1-processor_tres_escalas_o_mas-", self.config.processor_tres_escalas_o_mas_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-processor_dos_mas_rapidos-", self.config.processor_dos_mas_rapidos_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-processor_distancias-", self.config.processor_distancias_replicas, processor_checkers)
+        self.init_checker("tp1-processor_max_avg-", self.config.processor_max_avg_replicas, processor_checkers)
+        self.init_checker("tp1-processor_media_general-", self.config.processor_media_general_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-tagger_dos_mas_rapidos-", self.config.tagger_dos_mas_rapidos_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-tagger_tres_escalas_o_mas-", self.config.tagger_tres_escalas_o_mas_replicas,
+                          processor_checkers)
+        self.init_checker("tp1-tagger_distancias-", self.config.tagger_distancias_replicas, processor_checkers)
+        self.init_checker("tp1-tagger_max_avg-", self.config.tagger_max_avg_replicas, processor_checkers)
+        self.init_checker("tp1-load_balancer-", self.config.load_balancer_replicas, processor_checkers)
+        self.init_checker("tp1-grouper_", self.config.grouper_replicas, processor_checkers, processor_suffix="-1")
+        self.init_checker("tp1-joiner_", self.config.joiner_replicas, processor_checkers, processor_suffix="-1")
 
         # Wait for the processes to finish
         for processor_checker in processor_checkers:
             processor_checker.join()
             logging.info("Processor checker finished")
+
+    def init_checker(self, processor_prefix, replicas, processor_checkers, processor_suffix=""):
+        for i in range(1, replicas + 1):
+            processor_checker = Process(target=self.check_processor,
+                                        args=(f"{processor_prefix}{i}{processor_suffix}",))
+            processor_checker.start()
+            processor_checkers.append(processor_checker)
 
     def check_processor(self, processor_name):
         """
