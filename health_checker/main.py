58a2d8f6bb47a656686d5adddc1c74be258f39a5
Lucas Sotelo <lucasnsotelog@gmail.com>
2023-11-26 14:28:34 -0300

    feat: all processors in health checker

--- health_checker/main.py
+++ health_checker/main.py
@@ -8,6 +8,23 @@ def main():
         "logging_level": str,
         "filter_general_replicas": int,
         "filter_multiple_replicas": int,
+        "filter_avg_max_replicas": int,
+        "filter_distancia_replicas": int,
+        "filter_tres_escalas_o_mas_replicas": int,
+        "filter_dos_mas_rapidos_replicas": int,
+        "filter_lat_long_replicas": int,
+        "processor_tres_escalas_o_mas_replicas": int,
+        "processor_dos_mas_rapidos_replicas": int,
+        "processor_distancias_replicas": int,
+        "processor_max_avg_replicas": int,
+        "processor_media_general_replicas": int,
+        "tagger_dos_mas_rapidos_replicas": int,
+        "tagger_tres_escalas_o_mas_replicas": int,
+        "tagger_distancias_replicas": int,
+        "tagger_max_avg_replicas": int,
+        "load_balancer_replicas": int,
+        "grouper_replicas": int,
+        "joiner_replicas": int,
     }
     config_params = initialize_config(config_inputs)
 
@@ -17,6 +34,23 @@ def main():
     config = HealthCheckerConfig(
         config_params["filter_general_replicas"],
         config_params["filter_multiple_replicas"],
+        config_params["filter_avg_max_replicas"],
+        config_params["filter_distancia_replicas"],
+        config_params["filter_tres_escalas_o_mas_replicas"],
+        config_params["filter_dos_mas_rapidos_replicas"],
+        config_params["filter_lat_long_replicas"],
+        config_params["processor_tres_escalas_o_mas_replicas"],
+        config_params["processor_dos_mas_rapidos_replicas"],
+        config_params["processor_distancias_replicas"],
+        config_params["processor_max_avg_replicas"],
+        config_params["processor_media_general_replicas"],
+        config_params["tagger_dos_mas_rapidos_replicas"],
+        config_params["tagger_tres_escalas_o_mas_replicas"],
+        config_params["tagger_distancias_replicas"],
+        config_params["tagger_max_avg_replicas"],
+        config_params["load_balancer_replicas"],
+        config_params["grouper_replicas"],
+        config_params["joiner_replicas"]
     )
 
     HealthChecker(config).run()
